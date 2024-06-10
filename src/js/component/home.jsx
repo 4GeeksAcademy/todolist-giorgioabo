538856394cf6085dd8405d6b7e7c9477fbdf0cb0
Giorgio <155197460+giorgioabo@users.noreply.github.com>
2024-06-10 16:57:55 +0000

    todoslist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [newTodos, setNewTodos] = useState("")
+	const [todos, setTodos] = useState(["Make the bed", "Wash my hands", "Eat", "Walk the dog"])
+
+	const actValueNewTodos = (event) => {
+		setNewTodos(event.target.value)
+	}
+
+	const addNewTodos = (event) => {
+		event.preventDefault()
+		setTodos([...todos, newTodos])
+		setNewTodos("")
+	}
+
+	const deleteElements = (index) => {
+		/*const newArray = []
+		for (let i = 0; i < todos.length; i++) {
+			if (index !== i) newArray.push(todos[i])*/
+		const arrayModified = todos.filter((item, i) => i !== index)
+		setTodos(arrayModified)
+	}
+	/*setTodos(newArray)*/
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="w-50 m-auto">
+			<form onSubmit={addNewTodos}>
+				<div className="mb-3">
+					<div className="header text-center mb-3 fs-1 ">
+						todos
+					</div>
+					<input className="form-control" aria-describedby="emailHelp" value={newTodos} onChange={actValueNewTodos} />
+				</div>
+				<button type="submit" className="btn btn-outline-light mb-3">Submit</button>
+			</form>
+			<ul>
+				{
+					todos.map((item, index) => {
+						return (<li className="lista" key={index}>{item} <button className="btn text-danger x" onClick={() => deleteElements(index)}><i className="fa-solid fa-x"></i></button></li>)
+					})
+				}
+			</ul>
+			{todos.length === 0 ? <span className="tasks">No tasks, add a task</span> : <span className="tasks">{todos.length} item left</span>}
 		</div>
 	);
 };
